2f0b436bc9668e177e26cf05a0c08bd2707c3d6f
alameen767 <alameen767@gmail.com>
2021-12-06 14:12:24 +0530

    updated

--- form/settings.py
+++ form/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zkm2xg05*i+35gn4u#ut-+vqrm+5hn8j)av)j5$57$+%k@d2ds
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
